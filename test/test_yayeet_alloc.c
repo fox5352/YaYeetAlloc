5c4a30b003dfdc1e6a953e4309eea940d1b95411
christopher <christophervos522@gmail.com>
2024-07-25 00:17:29 +0200

    added test for tribe_rent_free

--- test/test_yayeet_alloc.c
+++ test/test_yayeet_alloc.c
@@ -2,8 +2,7 @@
 #include <assert.h>
 #include <stdlib.h>
 
-//#include "../include/yayeet_alloc.h"
-#include <yayeet_alloc.h>
+#include "../include/yayeet_alloc.h"
 
 void test_allocation_and_deallocation(){
     // testing normal memory allocation
@@ -62,6 +61,7 @@ void test_allocation_and_deallocation(){
 }
 
 void test_program_break() {
+
     void *initial_break = sbrk(0);
 
     void *ptr = rent_free(100000);
@@ -75,12 +75,35 @@ void test_program_break() {
     void *final_break = sbrk(0);
     assert(final_break == initial_break);
 
-    printf("Program break unit passed\n");
+    printf("Program break unit for rent_free passed\n");
+
+    void *initial_break2 = sbrk(0);
+
+    void *ptr2 = tribe_rent_free(4, 100000);
+    assert(ptr2 != NULL);
+
+    void *new_break2 = sbrk(0);
+    assert(new_break2 > initial_break2);
+
+    yeet(ptr2);
+
+    void *final_break2 = sbrk(0);
+    assert(final_break2 == initial_break2);
+
+    printf("Program break unit for tribe_rent_free passed\n");
 }
 
-void* thread_func(void *arg) {
+void* thread_func_rent_free(void *arg) {
     for (int i = 0; i < 1000; i++) {
         void *ptr = rent_free(100);
+        yeet(ptr);
+    }
+    return NULL;
+}
+
+void* thread_func_tribe_rent_free(void * arg) {
+    for (int i = 0; i < 1000; i++) {
+        void *ptr = tribe_rent_free(4, 100);
         yeet(ptr);
     }
     return NULL;
@@ -90,14 +113,24 @@ void test_thread_safety() {
     pthread_t threads[10];
 
     for (int i = 0; i < 10; i++) {
-        pthread_create(&threads[i], NULL, thread_func, NULL);
+        pthread_create(&threads[i], NULL, thread_func_rent_free, NULL);
     }
 
     for (int i = 0; i < 10; i++) {
         pthread_join(threads[i], NULL);
     }
+    printf("Thread safety unit for rent_free passed\n");
 
-    printf("Thread safety unit passed\n");
+    pthread_t threads2[10];
+
+    for (int i = 0; i < 10; i++) {
+        pthread_create(&threads2[i], NULL, thread_func_tribe_rent_free, NULL);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        pthread_join(threads2[i], NULL);
+    }
+    printf("Thread safety unit for tribe_rent_free passed\n");
 }
 
 void test_edge_cases() {
@@ -108,6 +141,17 @@ void test_edge_cases() {
     assert(ptr == NULL);
 
     yeet(NULL);  // Should not crash
+
+    printf("Edge cases for rent_free unit passed");
+
+    void *ptr2 = tribe_rent_free(0, 0);
+    assert(ptr2 == NULL);
+
+    ptr2 = tribe_rent_free(4, (size_t)-1);
+    assert(ptr2 == NULL);
+
+    yeet(NULL);  // Should not crash
+    printf("Edge cases for tribe_rent_free unit passed");
 }
 
 int main() {
